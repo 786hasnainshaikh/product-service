6c1c2a0772e59db528cc9483ab71194e8cd38014
Muhammad Hasnain <shaikhhussnain1122@gmail.com>
2026-04-05 19:42:19 +0500

    Track ProductController and all project files

--- src/main/java/com/product_service/Product/Service/Controller/Service/ProductService.java
+++ src/main/java/com/product_service/Product/Service/Controller/Service/ProductService.java
@@ -1,6 +1,7 @@
 package com.product_service.Product.Service.Controller.Service;
 
 import org.modelmapper.ModelMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import com.product_service.Product.Service.Entity.*;
 import com.product_service.Product.Service.Entity.Model.Request.ProductRequestDTO;
 import com.product_service.Product.Service.Entity.Model.Response.ApiResponse;
 import com.product_service.Product.Service.Entity.Model.Response.ProductResponseDTO;
+
 
 
 
